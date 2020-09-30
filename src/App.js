8cbf3307209ca00f0c50369690d9efac9b5c114d
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-30 16:43:25 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MenuMakanan from './Page/MenuMakanan';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Tugas React js 5</h1>
+    <MenuMakanan />      
+    </div>
+  );
+}
+
+export default App;
